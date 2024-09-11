1e1abe80c4e2fa5e7a9212eabb9bba702b19f0eb
Tiago Canha <canha.tiago@muffag.ch>
2024-09-11 17:37:37 +0200

    add task with js

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 let btnAdd = document.getElementById("buttonAdd");
 
-btnAdd.addEventListener('click',handleClick);
+btnAdd.addEventListener("click", handleClick);
 
-
-function handleClick(){
-    console.log('hoi t');
+function handleClick() {
+  let li = document.createElement("li");
+  li.classList.add(
+    "font-light",
+    "text-base",
+    "rounded-full",
+    "border-2",
+    "border-solid",
+    "border-black",
+    "px-4",
+    "py-2"
+  );
+  li.appendChild(document.createTextNode("Task"));
+  ul.appendChild(li);
 }
+
+let ul = document.getElementById("list");
